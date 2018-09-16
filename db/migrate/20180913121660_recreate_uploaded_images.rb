4fb43677f252d51c017829ee6183b77a52e92628
Daniel von Atzigen <daniel@vatz.ch>
2018-09-16 10:24:35 +0200

    Fixed late Image recreation return
    Image recreation returned to early if it found a user with no avatar image. Fixed by replacing `return` with `next`.

--- db/migrate/20180913121660_recreate_uploaded_images.rb
+++ db/migrate/20180913121660_recreate_uploaded_images.rb
@@ -3,7 +3,7 @@ class RecreateUploadedImages < ActiveRecord::Migration[5.2]
     puts "--- remove this file after deploying to production"
 
     User.all.each do |user|
-      return unless user.avatar?
+      next unless user.avatar?
 
       user.avatar.recreate_versions!
       user.save!
