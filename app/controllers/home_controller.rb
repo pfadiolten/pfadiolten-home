341e79d0c78580f7de7a4fd2f8d8e00ebbf18bfe
Daniel von Atzigen <daniel@vatz.ch>
2020-09-29 10:32:59 +0200

    restrict albums on `/home` to 3 records

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
     @home.events   = policy_scope(Event.active)
     @home.articles = policy_scope(Article.limit(5)).order_by_release
-    @home.albums   = policy_scope(Album.all.limit(5))
+    @home.albums   = policy_scope(Album.limit(3))
 
     authorize @home.events
     authorize @home.articles
