38dcdfd91b98872b4c05f7da99bd03ec5f5c0b04
Daniel von Atzigen <daniel@vatz.ch>
2021-09-17 18:20:38 +0200

    Fix an error that's raised when updating an event

--- app/responders/application_responder.rb
+++ app/responders/application_responder.rb
@@ -1,4 +1,17 @@
 class ApplicationResponder < ActionController::Responder
-  include Responders::FlashResponder
   include Responders::HttpCacheResponder
+  include Responders::FlashResponder
+
+  def initialize *args
+    super
+  end
+
+  def resource_name
+    name = super
+    # Fixes a bug where the name is the hash of all the resources translations defined in the locale file.
+    # Not really sure if it's a bug or just the way it's supposed to work, and we're using the locales wrong.
+    # (daniel-va, 2021.09.17)
+    return name[:one] if name.is_a?(Hash) && name.has_key?(:one)
+    name
+  end
 end
